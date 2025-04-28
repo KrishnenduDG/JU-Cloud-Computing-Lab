0c8a388229058707829a8af19b4d325edda667d2
KrishnenduDG <krishnendu.kdg@gmail.com>
2025-04-28 14:09:25 +0530

    Minor bug fix

--- IPC/WebService/app.py
+++ IPC/WebService/app.py
@@ -11,7 +11,7 @@ def home():
 @app.route("/api/data", methods=["GET"])
 def add_data():
     n1 = request.args.get("number1")
-    n2 = request.args.get("number1")
+    n2 = request.args.get("number2")
 
     return jsonify({"result": int(n1) + int(n2)})
 
